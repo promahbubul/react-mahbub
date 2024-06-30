c5bf08f901b7462448a8c3fc70f440ff04d971b2
promahbubul <mahbublalam500@gmail.com>
2024-06-30 22:43:25 +0600

    Prject setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Name: Mahbubul Alam</h1>
+      <p>Email: mahbublalam500@gmail.com</p>
+      <button>Github</button>
+    </div>
+  );
+}
+
+export default App;
